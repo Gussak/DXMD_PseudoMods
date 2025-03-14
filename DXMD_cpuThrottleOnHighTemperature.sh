77e46f5539c7cb096f27254d9b6856697bf4bb74
gussak <ghussack@gmail.com>
2025-03-14 01:05:58 -0300

    DXMD_cpuThrottleOnHighTemperature.sh: more generic, can be used w/o DXMD;

--- DXMD_cpuThrottleOnHighTemperature.sh
+++ DXMD_cpuThrottleOnHighTemperature.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+egrep "[#]help" "$0"
+
 function FUNCcheckIfGameIsRunning() {
 	pgrep -fa "retail/DXMD.exe -benchmark" >/dev/null
 }
+function FUNCecho() {
+	echo "EXEC: $@"
+	"$@"
+}
 
+fMinFreqDetected=$(cpupower frequency-info |grep limits |awk '{print $3}')
+declare -p fMinFreqDetected
+: ${fMinFreq:=${fMinFreqDetected}} #help your cpu minimum frequency (auto detected, but must be configured with visudo)
 while true;do
 	nTmpr=$(sensors |grep Tctl |awk '{print $2}' |cut -d. -f1);
-	if((nTmpr > 72));then
+	: ${nTmprLim:=72} #help temperature limit to throttle CPU down
+	if((nTmpr > nTmprLim));then
 		date
 		declare -p nTmpr
-		set -x;sudo cpupower frequency-set --min 1.55GHz --max 1.55GHz;set +x
+		set -x
+		FUNCecho \
+			sudo cpupower frequency-set --min ${fMinFreq}GHz --max ${fMinFreq}GHz #help this is the command you must set with visudo
+		set +x 
 	fi
+	
 	echoc -w -t 3 -n "\r$(date)"
-	if ! FUNCcheckIfGameIsRunning;then break;fi
+	
+	: ${bExitWithGame:=false} #help
+	if $bExitWithGame && ! FUNCcheckIfGameIsRunning;then break;fi
 done
 
